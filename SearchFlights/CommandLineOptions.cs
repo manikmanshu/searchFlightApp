24e835fe8302306612e2a56f382b685716835c75
manikmanshu <manikmanshu@gmail.com>
2018-05-26 23:49:18 +0530

    add flights details model and command line options model

---
+++ SearchFlights/CommandLineOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace SearchFlights
+{
+    public class CommandLineOptions
+    {
+        [Option('o', "origin", Required = true, HelpText = "Origin")]
+        public string Origin { get; set; }
+
+        [Option('d', "destination", Required = true, HelpText = "destination")]
+        public string Destination { get; set; }
+    }
+}
---
+++ SearchFlights/FlightDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SearchFlights
+{
+    public class FligtDetails
+    {
+        public string Origin { get; set; }
+
+        public string Destination { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public DateTime DestinationTime { get; set; }
+
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3} {4}", Origin, DepartureTime, Destination, DestinationTime, Price);
+        }
+
+    }
+}
