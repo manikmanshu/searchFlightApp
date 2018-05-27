b2827fe9e04bd6fcef52f27b81acccf96b2df56d
manikmanshu <manikmanshu@gmail.com>
2018-05-27 12:22:41 +0530

    add filter flights as per user options

---
+++ SearchFlights/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace SearchFlights
+{
+    /// <summary>
+    /// Application Constants
+    /// </summary>
+    public static class Constants
+    {        
+        public const string FlightOutput = "{0} --> {1} ({2} --> {3}) - ${4}";
+    }
+}
---
+++ SearchFlights/Utility/FilterFlights.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SearchFlights
+{
+    /// <summary>
+    /// Filter Flights
+    /// static class
+    /// </summary>
+    public static class FilterFlights
+    {
+        /// <summary>
+        /// Get Flights filtered as per options passed
+        /// </summary>
+        /// <param name="flights">list of IFlightDetails</param>
+        /// <param name="cmdOptions">CommandLineOptions</param>
+        /// <returns>List<IFlightDetails></returns>
+        public static List<IFlightDetails> GetFlights(List<IFlightDetails> flights, CommandLineOptions cmdOptions)
+        {
+            List<IFlightDetails> filtered = flights
+                .Where(flight => flight.Origin.Equals(cmdOptions.Origin) && flight.Destination.Equals(cmdOptions.Destination))
+                .Distinct()
+                .ToList();
+
+            return filtered;
+        }
+
+    }
+}
