d5eb06c42e1650abcc5d7310ebdd2e3cdb45a7c2
manikmanshu <manikmanshu@gmail.com>
2018-05-27 12:20:41 +0530

    add flight detail equality comparer

---
+++ SearchFlights/Models/FlightDetailEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace SearchFlights
+{
+    /// <summary>
+    /// FlightDetailEqualityComparer check for duplicates
+    /// </summary>
+    public class FlightDetailEqualityComparer : IEqualityComparer<IFlightDetails>
+    {
+        /// <summary>
+        /// Implement Equals Method
+        /// </summary>
+        /// <param name="x">IFlightDetails</param>
+        /// <param name="y">IFlightDetails</param>
+        /// <returns>bool</returns>
+        public bool Equals(IFlightDetails x, IFlightDetails y)
+        {
+            return x.Equals(y);
+        }
+
+        /// <summary>
+        /// Implements GetHashCode Method
+        /// </summary>
+        /// <param name="flightDetails">IFlightDetails</param>
+        /// <returns>int</returns>
+        public int GetHashCode(IFlightDetails flightDetails)
+        {
+            return flightDetails.ToString().GetHashCode();
+        }
+    }
+}
